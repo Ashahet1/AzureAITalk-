d436e367d36b0c4f82159ff875f30936fa9c339c
Riddhi Shah <shahriddhi717@gmail.com>
2026-02-10 11:50:15 -0500

    Delete DiagramAnalyzer.Core directory

--- DiagramAnalyzer.Core/Services/IGptVisionService.cs
+++
@@ -1,8 +0,0 @@
-using DiagramAnalyzer.Core.Models;
-
-namespace DiagramAnalyzer.Core.Services;
-
-public interface IGptVisionService
-{
-    Task<string> AnalyzeDiagramStructureAsync(byte[] imageData, List<ExtractedText> extractedText);
-}
